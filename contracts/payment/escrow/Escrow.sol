9a22693c1fdb20018ebce9f9a5342d4bb44a4dc0
Matej Nikorovič <matej.nikorovic@vacuumlabs.com>
2018-11-29 08:00:17 +0100

    Solidity scripts version upgrade 0.4.24->0.5.0

--- contracts/payment/escrow/Escrow.sol
+++ contracts/payment/escrow/Escrow.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.0;
 
 import "../../math/SafeMath.sol";
 import "../../ownership/Secondary.sol";
@@ -42,12 +42,12 @@ contract Escrow is Secondary {
     * @dev Withdraw accumulated balance for a payee.
     * @param payee The address whose funds will be withdrawn and transferred to.
     */
-    function withdraw(address payee) public onlyPrimary {
+    function withdraw(address payable payee) public onlyPrimary {
         uint256 payment = _deposits[payee];
 
         _deposits[payee] = 0;
 
-        payee.transfer(payment);
+        address(payee).transfer(payment);
 
         emit Withdrawn(payee, payment);
     }
